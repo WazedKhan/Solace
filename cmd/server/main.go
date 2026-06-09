9a178584987b4c81c2840ed9db927376d444683a
wazedkhan <wazedkhan119399@gmail.com>
2026-06-09 22:06:07 +0600

    feat: logging endpoint call

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/WazedKhan/Solace/db"
 	"github.com/WazedKhan/Solace/internal/auth"
+	"github.com/WazedKhan/Solace/middleware"
 )
 
 func main() {
@@ -28,6 +29,8 @@ func main() {
 	mux.HandleFunc("/api/v1/register", authHandler.Register)
 	mux.HandleFunc("/api/v1/users", authHandler.GetUsers)
 
+	middleware := middleware.RequestLog(mux)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
@@ -40,9 +43,14 @@ func main() {
 
 	addr := ":" + port
 
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: middleware,
+	}
+
 	log.Printf("server starting at http://%s:%s", server, port)
 
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
