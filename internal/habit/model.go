565486cb38b4ab2c60b53e8134cb039bc66ac6e0
wazedkhan <wazedkhan119399@gmail.com>
2026-06-17 21:48:28 +0600

    feat: repo layer for habits

---
+++ internal/habit/model.go
@@ -0,0 +1,41 @@
+package habit
+
+import (
+	"time"
+
+	"github.com/WazedKhan/Solace/models"
+)
+
+type Habit struct {
+	ID            string
+	Title         string
+	UserID        string
+	ImageUrl      *string
+	CurrentStreak int
+	LastCheckedAt *time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type HabitResponse struct {
+	ID            string    `json:"id"`
+	Title         string    `json:"title"`
+	ImageUrl      *string   `json:"image_url,omitempty"`
+	CurrentStreak int       `json:"current_streak"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+type HabitRequest struct {
+	Title    string  `json:"title"`
+	ImageUrl *string `json:"image_url"`
+}
+
+type CheckInResponse struct {
+	CurrentStreak int `json:"current_streak"`
+}
+
+type HabitQueryRequest struct {
+	QueryParams models.QueryParams
+	UserID      string
+}
---
+++ models/query.go
@@ -0,0 +1,6 @@
+package models
+
+type QueryParams struct {
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+}
